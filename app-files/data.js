66592cb434b029cee1270fc0665123e1e410ef1e
Enriquezwere <enriquez.marieleanne@gmail.com>
2023-10-15 11:39:35 -0800

    Ground Tour

---
+++ app-files/data.js
@@ -0,0 +1,625 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-ground-1",
+      "name": "Ground 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1767.5,
+      "initialViewParameters": {
+        "yaw": 0.19056145914631983,
+        "pitch": 0.04957383516662439,
+        "fov": 1.3563401604162861
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.05601984118772485,
+          "pitch": 0.31032230117873105,
+          "rotation": 0,
+          "target": "1-ground-2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.3265154667617693,
+          "pitch": 0.01269317930067615,
+          "title": "CSTC Chapel",
+          "text": "Church / chapel give responsibility to teach to student charged to teaching knowledge, teach about the faith All schools are various faith traditions especially&nbsp; Roman Catholic.&nbsp;<div><br></div>"
+        },
+        {
+          "yaw": 0.14179023132728474,
+          "pitch": 0.2756684418254345,
+          "title": "CSTC Canteen",
+          "text": ""
+        }
+      ]
+    },
+    {
+      "id": "1-ground-2",
+      "name": "Ground 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1767.5,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.113941003582143,
+          "pitch": -0.21407796093718545,
+          "rotation": 0,
+          "target": "0-ground-1"
+        },
+        {
+          "yaw": 1.4526239182611986,
+          "pitch": 0.22216053266991764,
+          "rotation": 0,
+          "target": "2-canteen"
+        },
+        {
+          "yaw": -2.1471229271843164,
+          "pitch": 0.17571125617210726,
+          "rotation": 0,
+          "target": "3-ground-4"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.5542347062522488,
+          "pitch": 0.014020074573487307,
+          "title": "CSTC Canteen",
+          "text": "School canteen provides healthy, safe and nutritious food for school community. Having proper management of school opportunities for the student to select canteen created prevent of unhealthy foods in school<div><br></div>"
+        }
+      ]
+    },
+    {
+      "id": "2-canteen",
+      "name": "Canteen",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.1255638626250075,
+          "pitch": 0.10917697888096356,
+          "rotation": 0,
+          "target": "1-ground-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-ground-4",
+      "name": "Ground 4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.08585562694052129,
+        "pitch": 0.19828502625300182,
+        "fov": 1.3563401604162861
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.6457057742926757,
+          "pitch": 0.15258908891877354,
+          "rotation": 0,
+          "target": "1-ground-2"
+        },
+        {
+          "yaw": 0.14911387866893477,
+          "pitch": 0.20952659082226788,
+          "rotation": 0,
+          "target": "4-ground-5"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.73966113354345,
+          "pitch": -0.008804633313344468,
+          "title": "Business Center",
+          "text": "Business center it is a room provides all students and staff&nbsp; that needs in school like uniform , booklet, CSTC template and etc. This is a space for all things needed for the activities of students to do<div><br></div>"
+        },
+        {
+          "yaw": 1.4085907216248206,
+          "pitch": 0.06003314915655977,
+          "title": "CSTC uniforms",
+          "text": ""
+        }
+      ]
+    },
+    {
+      "id": "4-ground-5",
+      "name": "Ground 5",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1767.5,
+      "initialViewParameters": {
+        "yaw": 0.08864279421138122,
+        "pitch": 0.11582894113237785,
+        "fov": 1.3563401604162861
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.7474175249387063,
+          "pitch": 0.17214087073107542,
+          "rotation": 0,
+          "target": "3-ground-4"
+        },
+        {
+          "yaw": -0.023560959619109312,
+          "pitch": 0.03826433426995557,
+          "rotation": 0,
+          "target": "5-ground-6"
+        },
+        {
+          "yaw": -1.642149335129922,
+          "pitch": -0.39561650853516106,
+          "rotation": 0,
+          "target": "9-ground-10"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.2834958841100894,
+          "pitch": -0.1701626103997036,
+          "title": "CSTC Comfort room",
+          "text": ""
+        },
+        {
+          "yaw": -0.761734531794465,
+          "pitch": 0.09410815947867235,
+          "title": "Culinary Kitchen",
+          "text": "School kitchen area for preparation serving of foods. If they have activities&nbsp; in the school kitchen room&nbsp; like need to cook and bake it can be used.<br><div><br></div>"
+        }
+      ]
+    },
+    {
+      "id": "5-ground-6",
+      "name": "Ground 6",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.5384141693097533,
+          "pitch": 0.1894575272556409,
+          "rotation": 0,
+          "target": "4-ground-5"
+        },
+        {
+          "yaw": -0.05003452587330415,
+          "pitch": -0.029411297834437633,
+          "rotation": 0,
+          "target": "6-ground-7"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.3962485002401408,
+          "pitch": 0.08999270607248633,
+          "title": "EIM NCII",
+          "text": "the electrical students take the NCII training which allows them to gain practical, and hands-on experience. It has safety features and proper equipment that ensures a safe learning environment for electrical systems training.<div><br></div>"
+        }
+      ]
+    },
+    {
+      "id": "6-ground-7",
+      "name": "Ground 7",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1767.5,
+      "initialViewParameters": {
+        "yaw": 0.03561896432641376,
+        "pitch": -0.032222992858306654,
+        "fov": 1.3563401604162861
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.118985951808119,
+          "pitch": 0.42700727488977286,
+          "rotation": 0,
+          "target": "5-ground-6"
+        },
+        {
+          "yaw": 0.08530588095919711,
+          "pitch": -0.365209252896733,
+          "rotation": 0,
+          "target": "7-ground-8"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.5576581788085315,
+          "pitch": 0.02188588905699973,
+          "title": "Assessment area",
+          "text": "This area is only for electrical strands to equip their knowledge for hands-on electrical training. Here students are free to practice their learning skills on electrical&nbsp;<div><br></div>"
+        }
+      ]
+    },
+    {
+      "id": "7-ground-8",
+      "name": "Ground 8",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1767.5,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.449500426355746,
+          "pitch": 0.5272569896188628,
+          "rotation": 0,
+          "target": "6-ground-7"
+        },
+        {
+          "yaw": -0.20088681648420348,
+          "pitch": 0.25900054899942404,
+          "rotation": 0,
+          "target": "8-ground-9"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-ground-9",
+      "name": "Ground 9",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1767.5,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.824701196205078,
+          "pitch": 0.1946071336758184,
+          "rotation": 0,
+          "target": "7-ground-8"
+        },
+        {
+          "yaw": -0.017073650292477538,
+          "pitch": 0.10506660275932944,
+          "rotation": 0,
+          "target": "11-exit"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-ground-10",
+      "name": "Ground 10",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1767.5,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.1081891430601054,
+          "pitch": 0.07300080287209454,
+          "rotation": 0,
+          "target": "10-ground-11"
+        },
+        {
+          "yaw": 2.5200767955742887,
+          "pitch": 0.5272280079636236,
+          "rotation": 0,
+          "target": "4-ground-5"
+        },
+        {
+          "yaw": -0.17913598886325843,
+          "pitch": 0.19602602865155738,
+          "rotation": 0,
+          "target": "11-exit"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.6646829702666714,
+          "pitch": -0.03783617090034497,
+          "title": "CSTC Computer rooms&nbsp;",
+          "text": "This hallway is the way to the Computer lab. The equipment is new and all working and opposite it is the I.T lab. All rooms are air-conditioned.<div><br></div>"
+        }
+      ]
+    },
+    {
+      "id": "10-ground-11",
+      "name": "Ground 11",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1767.5,
+      "initialViewParameters": {
+        "yaw": 0.12044175409903168,
+        "pitch": 0.27792715701551884,
+        "fov": 1.3563401604162861
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.810373541429623,
+          "pitch": 0.2263218793976094,
+          "rotation": 0,
+          "target": "9-ground-10"
+        },
+        {
+          "yaw": -3.1240033719931617,
+          "pitch": 0.11325286816665958,
+          "rotation": 0,
+          "target": "11-exit"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "11-exit",
+      "name": "Exit",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1767.5,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.075734488193728,
+          "pitch": 0.1037720140275411,
+          "rotation": 0,
+          "target": "10-ground-11"
+        },
+        {
+          "yaw": 1.5649601564418756,
+          "pitch": 0.06213144178436103,
+          "rotation": 0,
+          "target": "8-ground-9"
+        },
+        {
+          "yaw": -3.0129881099446347,
+          "pitch": 0.13627885967563458,
+          "rotation": 0,
+          "target": "9-ground-10"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.40613730044001706,
+          "pitch": 0.11770052983317747,
+          "title": "CSTC Exit",
+          "text": "The exit area is the way where students, and also teachers will go outside the school. There's always guard in this area to observe who's going out to leave the school premises, and to provide smooth ways of exiting, as it ensures the security and protection of CSTC institution.<div><br></div>"
+        }
+      ]
+    }
+  ],
+  "name": "Ground",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
